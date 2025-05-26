2a1437dfdfddb1962cdf7496243f390296f08c0e
pollakravenhorst306 <pollakravenhorst306@gmail.com>
2025-05-26 23:06:43 +0000

    Add 38.js

---
+++ src/38.js
@@ -0,0 +1,12 @@
+// Node.js example: A simple calculator program that performs addition
+
+// Function to add two numbers
+function sum(a, b) {
+  return a + b;
+}
+
+// Main function to test the sum function with some sample numbers
+const num1 = 4.5;
+const num2 = 6.7;
+
+console.log(`The result of adding ${num1} and ${num2} is: ${sum(num1, num2)}`);
